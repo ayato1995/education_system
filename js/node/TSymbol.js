38543ee95f8c73b07c0fdbe730c427ac5f311042
ayato1995 <ayato.k070206@outlook.jp>
2018-11-24 18:17:56 +0900

    TSymbol.jsに画面出力用のプロパティを追加

--- js/node/TSymbol.js
+++ js/node/TSymbol.js
@@ -2,8 +2,32 @@ var Terminal_symbol = enchant.Class.create(enchant.Sprite, {
   initialize: function(type) {
     enchant.Sprite.call(this, 16, 16);
     this.type = type;
+    /* 画面出力用のデータ */
+    this.image;
+    this.x;
+    this.y;
+    this.backgroundColor;
+
   },
 
+  /* 画面出力用のデータのsetter */
+  set_image: function(img) {
+    this.image = img;
+  },
+
+  set_x: function(x) {
+    this.x = x;
+  },
+
+  set_y: function(y) {
+    this.y = y;
+  },
+
+  set_backgroundColor: function(color) {
+    this.backgroundColor = color;
+  },
+
+  /* コンソール出力用 */
   display: function() {
     console.log(this.type);
   },
