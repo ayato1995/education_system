42b351abd1359587d306b99a0f8921d857422d1c
ayato1995 <ayato.k070206@outlook.jp>
2018-11-24 18:08:07 +0900

    arg_typeを削除

--- js/node/TSymbol.js
+++ js/node/TSymbol.js
@@ -2,15 +2,6 @@ var Terminal_symbol = enchant.Class.create(enchant.Sprite, {
   initialize: function(type) {
     enchant.Sprite.call(this, 16, 16);
     this.type = type;
-    this.arg_type = null;
-  },
-
-  get_arg_id: function() {
-    return this.arg_type;
-  },
-
-  set_arg_id: function(id) {
-    this.arg_type = id;
   },
 
   display: function() {
