76cd4be75c4167359921421d48470d30719b49f0
ayato1995 <ayato.k070206@outlook.jp>
2018-11-28 20:43:31 +0900

    upにcreate_block関数を追加

--- js/node/up.js
+++ js/node/up.js
@@ -2,4 +2,15 @@ var Up = enchant.Class.create(Terminal_symbol, {
   initialize: function() {
     Terminal_symbol.call(this, "advance");
   },
+
+  create_block: function(stage, x, y) {
+    var up = new Up();
+    up.set_image(this.image);
+    up.set_x(x);
+    up.set_y(y);
+    up.set_backgroundColor(this.backgroundColor);
+    up.register_move(stage);
+    up.register_append(stage);
+    return up;
+  }
 });
