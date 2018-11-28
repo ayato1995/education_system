e13c09fba047e8e0ae6b97ca7931a6b7fca03269
ayato1995 <ayato.k070206@outlook.jp>
2018-11-28 20:46:57 +0900

    rotate_rightにcreate_block関数を追加

--- js/node/rotate_right.js
+++ js/node/rotate_right.js
@@ -2,4 +2,15 @@ var Rotate_right = enchant.Class.create(Terminal_symbol, {
   initialize: function() {
     Terminal_symbol.call(this, "rotate_right");
   },
+
+  create_block: function(stage, x, y) {
+    var rr = new Up();
+    rr.set_image(this.image);
+    rr.set_x(x);
+    rr.set_y(y);
+    rr.set_backgroundColor(this.backgroundColor);
+    rr.register_move(stage);
+    rr.register_append(stage);
+    return rr;
+  }
 });
