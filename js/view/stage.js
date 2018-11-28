4bf1a1fe6ae89090ebdb2850151de04fb106c1fe
ayato1995 <ayato.k070206@outlook.jp>
2018-11-28 21:05:05 +0900

    今の実装に合わせた

--- js/view/stage.js
+++ js/view/stage.js
@@ -3,26 +3,28 @@ var Stage = enchant.Class.create(enchant.Scene, {
     enchant.Scene.call(this);
     this.prog = new Prog();
     this.blocks = this.set_blocks();
+    // is_touch : ブロックを作れるタイミングを管理
+    this.is_touch = true;
   },
 
   set_blocks: function() {
     var b = [];
     b.push(new Up());
-    b[0].set_block(null, 10, 10, "red", this.prog);
+    b[0].set_block(null, 10, 10, "red", this);
     b.push(new Rotate_right());
-    b[1].set_block(null, 10, 47, "red", this.prog);
+    b[1].set_block(null, 10, 47, "red", this);
     b.push(new Rotate_left());
-    b[2].set_block(null, 10, 84, "red", this.prog);
+    b[2].set_block(null, 10, 84, "red", this);
     b.push(new Loop());
-    b[3].set_block(null, null, 10, 121, this.prog);
+    b[3].set_block(null, null, 10, 121, this);
     b.push(new Func_id("spead", 0));
-    b[4].set_block(null, 10, 195, "green", this.prog);
+    b[4].set_block(null, 10, 195, "green", this);
     b.push(new Func_id("heart", 0));
-    b[5].set_block(null, 10, 232, "green", this.prog);
+    b[5].set_block(null, 10, 232, "green", this);
     b.push(new Func_id("dia", 0));
-    b[6].set_block(null, 10, 269, "green", this.prog);
+    b[6].set_block(null, 10, 269, "green", this);
     b.push(new Func_id("clover", 0));
-    b[7].set_block(null, 10, 306, "green", this.prog);
+    b[7].set_block(null, 10, 306, "green", this);
     return b;
   },
 
