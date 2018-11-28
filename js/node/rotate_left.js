d0ed11273c3319939eb36de9a49ed424c5109918
ayato1995 <ayato.k070206@outlook.jp>
2018-11-28 20:49:54 +0900

    rotate_leftにcrate_block関数を追加

--- js/node/rotate_left.js
+++ js/node/rotate_left.js
@@ -2,4 +2,15 @@ var Rotate_left = enchant.Class.create(Terminal_symbol, {
   initialize: function() {
     Terminal_symbol.call(this, "rotate_left");
   },
+
+  create_block: function(stage, x, y) {
+    var rl = new Rotate_left();
+    rl.set_image(this.image);
+    rl.set_x(x);
+    rl.set_y(y);
+    rl.set_backgroundColor(this.backgroundColor);
+    rl.register_move(stage);
+    rl.register_append(stage);
+    return rl;
+  }
 });
