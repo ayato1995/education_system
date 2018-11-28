a473d767e2e54f4fb603ea035198868023c6408b
ayato1995 <ayato.k070206@outlook.jp>
2018-11-28 21:01:23 +0900

    func_idにcreate_block関数を追加

--- js/node/func_id.js
+++ js/node/func_id.js
@@ -5,6 +5,17 @@ var Func_id = enchant.Class.create(Terminal_symbol, {
     this.args = args;
   },
 
+  create_block: function(stage, x, y) {
+    var fi = new Func_id(this.name, this.args);
+    fi.set_image(this.image);
+    fi.set_x(x);
+    fi.set_y(y);
+    fi.set_backgroundColor(this.backgroundColor);
+    fi.register_move(stage);
+    fi.register_append(stage);
+    return fi;
+  },
+
   display: function() {
     console.log("type : " + this.type + " name : " + this.name);
   }
