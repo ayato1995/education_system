d0715639824b8ace9af28dbcaf778b7dc93d799c
ayato1995 <ayato.k070206@outlook.jp>
2018-11-28 20:51:32 +0900

    defalt_?のフィールドを削除 & ブロックを作成するためのイベントを追加 & デバッグプリントが多め

--- js/node/TSymbol.js
+++ js/node/TSymbol.js
@@ -5,25 +5,23 @@ var Terminal_symbol = enchant.Class.create(enchant.Sprite, {
     /* 画面出力用のデータ */
     this.image;
     this.x;
-    this.default_x;
     this.y;
-    this.default_y;
     this.backgroundColor;
+    /* 複製したnodeを管理する */
+    this.node;
     /* 連結リスト */
     this.prev = null;
     this.next = null;
   },
 
   /* 画面出力用のデータのsetter */
-  set_block: function(img, x, y, color, prog) {
+  set_block: function(img, x, y, color, stage) {
     this.set_image(img);
     this.set_x(x);
-    this.set_default_x(x);
     this.set_y(y);
-    this.set_default_y(y);
     this.set_backgroundColor(color);
-    this.register_move();
-    this.register_append(prog);
+    this.register_init_move(stage);
+    this.register_init_block_append(stage);
   },
 
   set_image: function(img) {
@@ -36,14 +34,6 @@ var Terminal_symbol = enchant.Class.create(enchant.Sprite, {
 
   set_y: function(y) {
     this.y = y;
-  },
-
-  set_default_x: function(x) {
-    this.default_x = x;
-  },
-
-  set_default_y: function(y) {
-    this.default_y = y;
   },
 
   set_backgroundColor: function(color) {
@@ -83,9 +73,13 @@ var Terminal_symbol = enchant.Class.create(enchant.Sprite, {
   },
 
   delete: function() {
+    console.log(this);
+    console.log(this.prev);
     if (this.next != null) {
+      console.log("tttttttt");
       this.next.set_prev(this.prev);
     }
+    console.log(this.prev);
     this.prev.set_next(this.next);
     var prev = this.prev;
     this.next = null;
@@ -99,43 +93,104 @@ var Terminal_symbol = enchant.Class.create(enchant.Sprite, {
   },
 
   /* イベントリスナ登録 */
-  register_move: function() {
+  /* ブロックを作成し作成したブロックを移動させるイベント */
+  register_init_move: function(stage) {
     this.addEventListener("touchmove", function(e) {
-      if (this.prev != null || this.next != null) {
-        if (this.x > this.prev.x + 32 || this.x < this.prev.x) {
-          this.delete();
+      var node = this.node;
+      if (stage.is_touch) {
+        node = this.create_block(stage);
+        stage.addChild(node);
+        this.node = node;
+        stage.is_touch = false;
+      }
+      if (node.prev != null || node.next != null) {
+        if (node.x > node.prev.x + 32 || node.x < node.prev.x) {
+          node.delete();
         }
       }
-      this.x = e.x;
-      this.y = e.y;
+      node.x = e.x;
+      node.y = e.y;
     });
   },
 
-  /* 連結リスト挿入用 */
-  register_append: function(prog) {
+  /* 作成したブロックを追加するためのイベント */
+  register_init_block_append: function(stage) {
     this.addEventListener("touchend", function(e) {
+      console.log(this.node.type);
+      stage.is_touch = true;
+      var prog = stage.prog
       if (prog.is_x_main_head_inside(e.x)) {
-        if (prog.is_y_main_head_inside(this, e.y))
+        if (prog.is_y_main_head_inside(this.node, e.y)) {
           return true;
+        }
       }
       if (prog.is_x_s_head_inside(e.x)) {
-        if (prog.is_y_s_head_inside(this, e.y))
+        if (prog.is_y_s_head_inside(this.node, e.y)) {
           return true;
+        }
       }
       if (prog.is_x_h_head_inside(e.x)) {
-        if (prog.is_y_h_head_inside(this, e.y))
+        if (prog.is_y_h_head_inside(this.node, e.y)) {
           return true;
+        }
       }
       if (prog.is_x_d_head_inside(e.x)) {
-        if (prog.is_y_d_head_inside(this, e.y))
+        if (prog.is_y_d_head_inside(this.node, e.y)) {
           return true;
+        }
       }
       if (prog.is_x_c_head_inside(e.x)) {
-        if (prog.is_y_c_head_inside(this, e.y))
+        if (prog.is_y_c_head_inside(this.node, e.y)) {
           return true;
+        }
       }
-      this.x = this.default_x;
-      this.y = this.default_y;
+      stage.removeChild(this.node);
+    });
+  },
+
+  register_move: function(stage) {
+    this.addEventListener("touchmove", function(e) {
+      if (this.prev != null || this.next != null) {
+        this.delete();
+      }
+      this.x = e.x;
+      this.y = e.y;
+    })
+  },
+
+
+  /* 連結リスト挿入用 */
+  register_append: function(stage) {
+    this.addEventListener("touchend", function(e) {
+      console.log(this.type);
+      //stage.is_touch = true;
+      var prog = stage.prog
+      if (prog.is_x_main_head_inside(e.x)) {
+        if (prog.is_y_main_head_inside(this, e.y)) {
+          return true;
+        }
+      }
+      if (prog.is_x_s_head_inside(e.x)) {
+        if (prog.is_y_s_head_inside(this, e.y)) {
+          return true;
+        }
+      }
+      if (prog.is_x_h_head_inside(e.x)) {
+        if (prog.is_y_h_head_inside(this, e.y)) {
+          return true;
+        }
+      }
+      if (prog.is_x_d_head_inside(e.x)) {
+        if (prog.is_y_d_head_inside(this, e.y)) {
+          return true;
+        }
+      }
+      if (prog.is_x_c_head_inside(e.x)) {
+        if (prog.is_y_c_head_inside(this, e.y)) {
+          return true;
+        }
+      }
+      stage.removeChild(this);
     });
   },
 
@@ -143,5 +198,5 @@ var Terminal_symbol = enchant.Class.create(enchant.Sprite, {
   register_remove: function(prog) {
     this.addEventListener("touchend", function(e) {
     });
-  }
+  },
 });
