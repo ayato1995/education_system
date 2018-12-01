f4c82c4d7ecc5e0d30482aefc38c0b9332f6f7b0
ayato1995 <ayato.k070206@outlook.jp>
2018-12-01 16:45:12 +0900

    ブロックを関数宣言の場所のみに移動できるように実装

--- js/node/param.js
+++ js/node/param.js
@@ -3,4 +3,39 @@ var Param = enchant.Class.create(Terminal_symbol, {
     Terminal_symbol.call(this, "param");
     this.id = id;
   },
+
+  create_block: function(stage, x, y) {
+    var p = new Param(this.id);
+    p.set_x(x);
+    p.set_y(y);
+    p.set_backgroundColor(this.backgroundColor);
+    p.register_move(stage);
+    p.register_append(stage);
+    p.register_above(stage);
+    return p;
+  },
+
+  block_append: function(prog, e) {
+    if (prog.is_x_s_head_inside(e.x)) {
+      if (prog.is_y_s_head_inside(this, e.y)) {
+        return true;
+      }
+    }
+    if (prog.is_x_h_head_inside(e.x)) {
+      if (prog.is_y_h_head_inside(this, e.y)) {
+        return true;
+      }
+    }
+    if (prog.is_x_d_head_inside(e.x)) {
+      if (prog.is_y_d_head_inside(this, e.y)) {
+        return true;
+      }
+    }
+    if (prog.is_x_c_head_inside(e.x)) {
+      if (prog.is_y_c_head_inside(this, e.y)) {
+        return true;
+      }
+    }
+    return false;
+  }
 });
