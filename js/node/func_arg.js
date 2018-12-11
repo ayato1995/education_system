d80f912645c4128212108e0c55503bfc228d1e33
ayato1995 <ayato.k070206@outlook.jp>
2018-12-11 23:38:25 +0900

    関数呼び出しの実行中のブロックを表示できるようにした（バグあり）

--- js/node/func_arg.js
+++ js/node/func_arg.js
@@ -1,40 +1,63 @@
-var Func_arg = function(id) {
-  this.type = "func_arg";
-  this.id = id;
-  this.stmts = [];
-}
+var Func_arg = enchant.Class.create({
+  initialize: function(id) {
+    this.type = "func_arg";
+    this.id = id;
+    this.stmts = [];
+  },
 
-Func_arg.prototype.get_id = function() {
-  return this.id;
-}
+  get_id: function() {
+    return this.id;
+  },
 
-Func_arg.prototype.push_stmt = function(stmt) {
-  this.stmts.push(stmt);
-}
+  push_stmt: function(stmt) {
+    this.stmts.push(stmt);
+  },
 
-Func_arg.prototype.get_stmts = function() {
-  return this.stmts;
-}
+  get_stmts: function() {
+    return this.stmts;
+  },
 
-Func_arg.prototype.get_stmt = function(i) {
-  return this.stmts[i];
-}
+  get_stmt: function(i) {
+    return this.stmts[i];
+  },
 
-Func_arg.prototype.get_stmts_length = function() {
-  return this.stmts.length;
-}
+  get_stmts_length: function() {
+    return this.stmts.length;
+  },
 
-Func_arg.prototype.display = function() {
-  console.log("arg" + this.id);
-  for (var i = 0; i < this.stmts.length; i++) {
-    if (this.stmts[i].type == "main_loop" ||
-      this.stmts[i].type == "func_loop" ||
-      this.stmts[i].type == "main_stmts" ||
-      this.stmts[i].type == "func_stmts") {
-      this.stmts[i].print_stmts();
-    } else {
-      this.stmts[i].display();
+  display_stmts: function(stage, x, y) {
+    var res = new Object();
+    res.x = x;
+    res.y = y;
+    for (var i = 0; i < this.stmts.length; i++) {
+      console.log(this.stmts[i].type);
+      if (this.stmts[i].type == "main_loop" ||
+        this.stmts[i].type == "main_stmts" ||
+        this.stmts[i].type == "func_call") {
+        res = this.stmts[i].display_stmts(stage, res.x, res.y);
+      } else {
+        this.stmts[i].set_x(res.x);
+        this.stmts[i].set_y(res.y);
+        this.stmts[i].set_backgroundColor("red");
+        stage.addChild(this.stmts[i]);
+        res.y += this.stmts[i].height + 5;
+      }
     }
-  }
-  console.log("end arg" + this.id);
-}
+    return res;
+  },
+
+  display: function() {
+    console.log("arg" + this.id);
+    for (var i = 0; i < this.stmts.length; i++) {
+      if (this.stmts[i].type == "main_loop" ||
+        this.stmts[i].type == "func_loop" ||
+        this.stmts[i].type == "main_stmts" ||
+        this.stmts[i].type == "func_stmts") {
+        this.stmts[i].print_stmts();
+      } else {
+        this.stmts[i].display();
+      }
+    }
+    console.log("end arg" + this.id);
+  },
+});
--- js/node/func_call.js
+++ js/node/func_call.js
@@ -1,20 +1,38 @@
-var Func_call = function(id) {
-  this.type = "func_call";
-  this.id = id;
-  this.func_args = [];
-}
+var Func_call = enchant.Class.create({
+  initialize: function(id) {
+    this.type = "func_call";
+    this.id = id;
+    this.func_args = [];
+  },
 
-Func_call.prototype.set_func_args = function(index, arg) {
-  this.func_args[index] = arg;
-}
+  set_func_args: function(index, arg) {
+    this.func_args[index] = arg;
+  },
 
-Func_call.prototype.get_args = function() {
-  return this.func_args;
-}
+  get_args: function() {
+    return this.func_args;
+  },
 
-Func_call.prototype.display = function() {
-  console.log("func_call : " + this.id);
-  for (var i = 0; i < this.func_args.length; i++) {
-    this.func_args[i].display();
-  }
-}
+  display_stmts: function(stage, x, y) {
+    var res = new Object();
+    res.x = x;
+    res.y = y;
+    var block = new Func_id(this.id);
+    block.set_x(x);
+    block.set_y(y);
+    block.set_backgroundColor("green");
+    stage.addChild(block);
+    res.y += block.height + 5;
+    for (var i = 0; i < this.func_args.length; i++) {
+      res = this.func_args[i].display_stmts(stage, res.x, res.y);
+    }
+    return res;
+  },
+
+  display: function() {
+    console.log("func_call : " + this.id);
+    for (var i = 0; i < this.func_args.length; i++) {
+      this.func_args[i].display();
+    }
+  },
+});
