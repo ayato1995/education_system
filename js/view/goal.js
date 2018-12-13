2c895fd4caf23e813a9ac8c683ab45699d715eaa
ayato1995 <ayato.k070206@outlook.jp>
2018-12-13 17:31:19 +0900

    goalの処理を間違えて消してた

--- js/view/goal.js
+++ js/view/goal.js
@@ -2,7 +2,7 @@ var Goal = enchant.Class.create(enchant.Sprite, {
   initialize: function(x, y) {
     enchant.Sprite.call(this, 16, 16);
     this.image = game.assets[GOAL];
-    console.log(x + " " + y);
+    this.x = x;
     this.y = y;
   },
 })
